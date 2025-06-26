01de7a4668800a8d2403d60d463a6a42576d5d55
中原麻理 <withusnakahara@gmail.com>
2025-06-26 15:54:41 +0900

    feat: Add /todo route to display Hello World message

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +15,8 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/todo', function () {
+    echo 'Hello World!';
 });
